2f99051f2fa9d523d185f8fec0804ea927b4bb0f
Sipho Mogolegang <smogolegang@gmail.com>
2026-07-07 11:55:19 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Loop in C Programming
+
+This program demonstrates loop structures in C.
+
+## How to Compile and Run
+
+1. Compile the program:
+   gcc loop.c -o loop.exe
+
+2. Run the executable:
+   .\loop.exe
+
+## Features
+- Demonstrates for loops
+- Shows while loops
+- Includes do-while loops
+
+## Author
+Your Name
+
+## License
+MIT
---
+++ loop.c
@@ -0,0 +1,19 @@
+
+//includes the standard input/out library for c to ble able to recieve and output data.
+
+#include <stdio.h>
+
+//The main entrance point is the main function.
+ int main() {
+
+// declare thew data type as integer and the variable as i
+int i;
+
+//i=0: for loop settings, inital value of variable i is 0.
+//i<10: trun the loop as long as the i variable has value less than 10, so up to 9.
+//i++:keep adding one to the current value of variable i.
+for(i=0; i<10; i++){
+//show this
+  printf("I am number: %d\n",i);
+}
+ }
